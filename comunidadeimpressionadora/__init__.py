4b23352be147ca02f1b1f2c5968554ef3699d25f
Dario-Yoshida <102775938+Dario-Yoshida@users.noreply.github.com>
2023-04-08 11:14:30 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -4,6 +4,9 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import inspect, Inspector
+
 
 app = Flask(__name__)
 
@@ -12,7 +15,7 @@ app.config['SECRET_KEY'] = 'a1f5105eaf0db9b3fb69853150250679'
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Yoiti/PycharmProjects/SiteComunidade/comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -20,16 +23,19 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
+
 from comunidadeimpressionadora import models
+
+
 engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspection = sqlalchemy.inspect(engine)
-if not inspection.has_table("usuario", schema="dbo"):
+ins = inspect(engine)
+existe = ins.dialect.has_table(engine.connect(), "usuario")
+if not existe:
     with app.app_context():
         database.drop_all()
         database.create_all()
-        print("A base de dados foi criada com sucesso")
+    print("Base de dados Criados com Sucesso!!")
 else:
-    print("A base de dados já existe")
-
+    print("Base de dados ja existentes")
 
 from comunidadeimpressionadora import routes
