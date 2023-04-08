e123aafc702215a0fbedf7f6b3b7765584fcdd2f
Dario-Yoshida <102775938+Dario-Yoshida@users.noreply.github.com>
2023-04-08 15:59:30 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a1f5105eaf0db9b3fb69853150250679'
 
 if os.getenv("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("postgresql://postgres:0fI9ifHp1KJkiCZ5Pyc0@containers-us-west-35.railway.app:7541/railway")
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
